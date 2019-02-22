9f5a8099000c22dad3fc7e212afac4e5f324399c
lakshyajitdk2 <lakshyajitdk2@gmail.com>
2019-02-23 02:51:48 +0530

    first version of Boolean.py

---
+++ Boolean.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 04:48:02 2018
+
+@author: lakshya
+"""
+
+#Type return  the type of data type
+print (type("Lakshya"))
+print(type(True))
+
+def c_greater_than_d_plus_e(c,d,e):
+#    if c > d + e :
+#        return True
+#    else:
+#        return False
+    return c > d + e
+
+#calling the function
+print (c_greater_than_d_plus_e(5,3,5))
+print (c_greater_than_d_plus_e(5,1,2))    
